f2159e25429cff031ebbbe87660b67b5b8794325
Edwin Arturo Castro Villamar <arturocastro98@hotmail.com>
2025-12-20 00:31:49 -0500

    Update auth.controller.ts

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -26,7 +26,7 @@ export const login = async (req: Request, res: Response) => {
   if (!match) return res.status(400).json({ message: "Invalid credentials" });
 
   const token = jwt.sign({ id: user._id, email: user.email }, env.JWT_SECRET, {
-    expiresIn: "1d",
+    expiresIn: "1h",
   });
 
   res.json({ token });
